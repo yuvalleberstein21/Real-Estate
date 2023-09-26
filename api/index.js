a39ac8bba3f177f8f4598169f11e9946b07a1869
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-09-26 22:00:40 +0300

    final sign up UI

--- api/index.js
+++ api/index.js
@@ -1,9 +1,36 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js"
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Connected to MongoDB 🌐✅");
+}).catch((err) => {
+    console.log(err)
+})
 
 const app = express();
 
+app.use(express.json());
 
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+// Middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000 ✅');
 });
+
