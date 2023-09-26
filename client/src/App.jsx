b2c1f95658692ef6c440f3439e8d0be6b1978f61
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-09-26 18:01:30 +0300

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline text-center">Hello world!</h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
