25bd5ee4d2beb8077eb2acb788fa3cbbfd1f6db1
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-09-27 19:32:12 +0300

    redux + redux persist

---
+++ client/src/redux/store.js
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+
+const rootReducer = combineReducers({ userReducer })
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: { user: persistedReducer },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    }),
+});
+
+
+export const persistor = persistStore(store);
