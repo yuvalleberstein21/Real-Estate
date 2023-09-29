14a99a265eb5fc514bf8fdf35098d2921fe7cba6
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-09-29 11:13:44 +0300

    profile functionlity

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 
 export const test = (req, res) => {
@@ -7,25 +8,30 @@ export const test = (req, res) => {
 }
 
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only update your own account !"));
-
+    if (req.user.id !== req.params.id)
+        return next(errorHandler(401, 'You can only update your own account!'));
     try {
         if (req.body.password) {
             req.body.password = bcrypt.hashSync(req.body.password, 10);
         }
 
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-            $set: {
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatarUrl: req.body.avatar,
-            }
-        }, { new: true })
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar: req.body.avatar,
+                },
+            },
+            { new: true }
+        );
 
-        const { password, ...rest } = updatedUser._doc
+        const { password, ...rest } = updatedUser._doc;
+
         res.status(200).json(rest);
     } catch (error) {
-        next(error)
+        next(error);
     }
 };
